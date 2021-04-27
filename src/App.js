3a6e3454ba508f9f9a1e54fde4fbbf8bde8f809e
longpatrick <long.patrick1997@gmail.com>
2021-04-27 16:28:15 -0500

    complete add function, connect to postgres via fetch in effect hook

--- src/App.js
+++ src/App.js
@@ -6,14 +6,49 @@ import SearchResults from './components/SearchResults/SearchResults'
 
 function App() {
 
-  const [ searchData, setSearchData ] = useState([])
-  const [ searchInput, setSearchInput ] = useState('')
+  const [ searchData, setSearchData ] = useState([]);
+  const [ searchInput, setSearchInput ] = useState('');
+  const [ getBooks, setBooks ] = useState({
+    readBooks: [],
+    notReadBooks: []
+  });
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const readBooks = await fetch('http://localhost:3000/already_reads')
+        .then(res => res.json());
+      const notReadBooks = await fetch('http://localhost:3000/want_to_reads')
+        .then(res => res.json());
+        setBooks({ readBooks, notReadBooks });
+    }
+
+    getBooks();
+  }, [])
 
   async function bookSearch() {
     if (searchInput !== '') {
       const newSearchResults = await searchByTitle(searchInput)
       setSearchData(newSearchResults.items)
       console.log(newSearchResults.items)
+    }
+  }
+
+  const handleAddToWants = async (event, formInputs) => {
+    try {
+      event.preventDefault();
+      const book = await fetch('http://localhost:3000/want_to_reads', {
+        body: JSON.stringify(formInputs),
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json());
+      setBooks(prevState => ({
+        notReadBooks: [book, ...prevState.notReadBooks]
+      }))
+    } catch (error) {
+      console.log(error);
     }
   }
 
