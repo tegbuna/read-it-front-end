14a0536f7c4838f4008eff5b486c8d575933c9e9
longpatrick <long.patrick1997@gmail.com>
2021-04-28 12:50:44 -0500

    boilerplate code for BookLists component

--- src/components/BookLists/BookLists.js
+++ src/components/BookLists/BookLists.js
@@ -1,21 +1,16 @@
-import { useState, useEffect } from 'react';
-
-function BookLists(props) {
-    const List1 = function() {
-        return (
+const BookLists = (props) => {
+    return (
+        <div>
           <ul className="lists-container">
             <li>List1</li>
           </ul>
-        );
-      };
-      
-      const List2 = function() {
-        return (
+          
           <ul className="lists-cotainer">
             <li>List2</li>
         
           </ul>
-        );
-      };
+        </div>
+    )
+}
 
 export default BookLists;
