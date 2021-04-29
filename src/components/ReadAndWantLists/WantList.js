f72272c4f5cb95a406d7310fb66ec9c21b808019
longpatrick <long.patrick1997@gmail.com>
2021-04-29 17:44:03 -0500

    add form for completing update function to DB; WIP

--- src/components/ReadAndWantLists/WantList.js
+++ src/components/ReadAndWantLists/WantList.js
@@ -1,15 +1,37 @@
 import styles from './Lists.module.css'
+import { useState } from 'react';
+import Form from '../Form/Form';
 
 const WantList = (props) => {
-    if (props.getBooks?.books.length > 0) {
+
+    const [editFormVisible, setEditFormVisible] = useState(false);
+
+    const toggleForm = () => {
+        setEditFormVisible(!editFormVisible);
+    };
+
+    if (props.getBooks.books.length > 0) {
         return props.getBooks.books.map((book, idx) => {
             if (book.have_read == false) {
                 return(
-                    <div className={styles.listItem}>
-                        <p>{book.title}</p>
-                        <p> by {book.author}</p>
-                        <button onClick={() => props.handleDelete(book.id)}>X</button>
-                    </div>
+                    <>
+                        {
+                            editFormVisible ? 
+                            <Form 
+                                editFormVisible={editFormVisible}
+                                toggleForm={toggleForm}
+                                handleUpdate={props.handleUpdate}
+                                book={book}
+                            />
+                            :
+                            <div className={styles.listItem}> 
+                                <p>{book.title}</p>
+                                <p> by {book.author}</p>
+                                <button onClick={() => props.handleDelete(book.id)}>X</button>
+                                <button onClick={toggleForm}>Edit</button>
+                            </div>
+                        }
+                    </>
                 )
             }
         })
