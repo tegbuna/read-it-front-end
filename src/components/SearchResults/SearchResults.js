894f0f9d398155c25283689ac92e8fb7b6e2d9bb
Owen Sullivan <owensullivan482@gmail.com>
2021-05-01 15:21:16 -0400

    search list styling

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,11 +4,15 @@ import SearchResult from '../SearchResult/SearchResult'
 const SearchResults = (props) => {
     if (props.searchData.length > 0) {
         return(
-            <div className={styles.searchResultsContainer}>
-                <SearchResult 
-                    searchData={props.searchData} 
-                    addBookToList={props.addBookToList}
-                />
+            <div className={styles.deskBackground}>
+                <div className={styles.notebookBackground}>
+                    <div className={styles.searchResultsContainer}>
+                        <SearchResult 
+                            searchData={props.searchData} 
+                            addBookToList={props.addBookToList}
+                        />
+                    </div>
+                </div>
             </div>
         )
     } else {
